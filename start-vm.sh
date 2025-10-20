cc7d138f8fb45df61be90d5d41dd94bf53741a94
Steven Miller <sjmiller609@gmail.com>
2025-10-20 14:58:00 -0400

    Delete old file

--- start-vm.sh
+++
@@ -1,7 +0,0 @@
-sudo cloud-hypervisor \
-	--kernel ./hypervisor-fw \
-	--disk path=focal-server-cloudimg-amd64.raw path=/tmp/ubuntu-cloudinit.img \
-	--cpus boot=4 \
-	--memory size=1024M \
-	--api-socket path=/tmp/cloud-hypervisor-test.sock \
-	--net "tap=,mac=,ip=,mask="
